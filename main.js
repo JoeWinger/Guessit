7a7d94d461a49a800d287b89e4f90fe185f39006
Joe Winger <joseph.winger@student.svpanthers.org>
2018-04-16 12:38:11 -0400

    Get posts and show a random one

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ var DEFAULT_SUBS = ['art', 'askreddit', 'aww', 'books', 'earthporn', 'food',
 				'jokes', 'mildlyinteresting', 'news', 'oldschoolcool', 'photoshopbattles',
 				'pics', 'science', 'showerthoughts', 'space', 'sports', 'videos', 'worldnews'];
 
-let API_URL = buildURL(subList);
+let API_URL = buildURL(DEFAULT_SUBS).concat("/hot.json?t=day&limit=" + DEFAULT_SUBS.length*10);
+
+var USABLE_POSTS = {};
 
 function buildURL(subList) {
-	const url = 'https://i.reddit.com/r/';
+	let url = 'https://i.reddit.com/r/';
 	for(let i = 0; i < subList.length; i++) {
 		url = url.concat("+" + subList[i]);
 	}
 	return url;
 }
 
-API_URL = API_URL.concat("/hot.json?t=day&limit=" + DEFAULT_SUBS.length*10);
-
 $.getJSON(API_URL, (res) => {
-	console.log(res);
-	var postTitle = res.data.children[0].data.title;
-	
-	$('#post-title').text(postTitle);
+	USABLE_POSTS = res.data.children;
+	showNewPost();
 });
 
+function getPost() {
+	let r = Math.floor(Math.random() * USABLE_POSTS.length);
+	let post = USABLE_POSTS[r];
+	USABLE_POSTS.splice(r, 1);
+	return post.data;
+}
+
+function showNewPost() {
+	$('#post-title').text(getPost().title);
+}
