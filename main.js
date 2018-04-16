ec1acc77c5f2767a4944c8b4dff8de49c4c16b68
Joe Winger <joseph.winger@student.svpanthers.org>
2018-04-16 12:21:56 -0400

    Abstract URL builder

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-let API_URL = 'https://i.reddit.com/r/';
-
 var DEFAULT_SUBS = ['art', 'askreddit', 'aww', 'books', 'earthporn', 'food',
 				'funny', 'gadgets', 'gifs', 'history', 'internetisbeautiful',
 				'jokes', 'mildlyinteresting', 'news', 'oldschoolcool', 'photoshopbattles',
 				'pics', 'science', 'showerthoughts', 'space', 'sports', 'videos', 'worldnews'];
 
-for(let i = 0; i < DEFAULT_SUBS.length; i++) {
-	console.log(DEFAULT_SUBS[i]);
-	API_URL = API_URL.concat("+" + DEFAULT_SUBS[i]);
+let API_URL = buildURL(subList);
+
+function buildURL(subList) {
+	const url = 'https://i.reddit.com/r/';
+	for(let i = 0; i < subList.length; i++) {
+		url = url.concat("+" + subList[i]);
+	}
+	return url;
 }
+
 API_URL = API_URL.concat("/hot.json?t=day&limit=" + DEFAULT_SUBS.length*10);
 
 $.getJSON(API_URL, (res) => {
@@ -17,3 +21,4 @@ $.getJSON(API_URL, (res) => {
 	
 	$('#post-title').text(postTitle);
 });
+
