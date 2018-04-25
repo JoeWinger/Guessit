732c8f61c20da641828e5d926a50a28022c0ebbe
Joe Winger <joseph.winger@student.svpanthers.org>
2018-04-25 13:06:38 -0400

    Clean the code a bunch

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 var DEFAULT_SUBS = ['art', 'askreddit', 'aww', 'books', 'earthporn', 'food',
 				'funny', 'gadgets', 'gifs', 'history', 'internetisbeautiful',
 				'jokes', 'mildlyinteresting', 'news', 'oldschoolcool', 'photoshopbattles',
-				'pics', 'science', 'showerthoughts', 'space', 'sports', 'videos', 'worldnews'];
+				'pics', 'science', 'showerthoughts', 'space', 'sports', 'videos', 'worldnews'],
+				USABLE_POSTS = {}, CURRENT_POST, points = 0, wrongs = 0;
+
 
 let API_URL = buildURL(DEFAULT_SUBS).concat("/hot.json?t=day&limit=" + DEFAULT_SUBS.length*10);
-
 $('#sublist').html(buildAutocompleteList(DEFAULT_SUBS));
-
-var USABLE_POSTS = {};
-
-var CURRENT_POST;
-
-var points = 0;
-var wrongs = 0;
+//This codes run asyncronously in the background to grab data from Reddit
+$.getJSON(API_URL, (res) => {
+	USABLE_POSTS = res.data.children;
+	showNewPost();
+});
+$('#btn-guess').click(() => { checkGuess($('#guess').val()); });
+$("#guess").keyup((e) => { if (e.keyCode === 13) checkGuess($('#guess').val()); });
 
 /*
- * Utility Functions
+ * Utilities
  */
+function checkGuess(guess) {
+	let correctGuess = CURRENT_POST.subreddit.toLowerCase();
+	guess = guess.toLowerCase();
+
+	if(guess == correctGuess) {
+		$('#guess').removeClass('incorrect').removeClass('correct').addClass('correct');
+		points++;
+		wrongs = 0;
+		showNewPost();
+	} else {
+		$('#guess').removeClass('correct').removeClass('incorrect').addClass('incorrect');
+		points--;
+		wrongs++;
+		if(wrongs == 3) {
+			wrongs = 0;
+			showNewPost();
+		}
+	}
+
+	$('#points').text(points);
+	$('#guess').val('');
+}
 function buildURL(subList) {
 	let url = 'https://i.reddit.com/r/';
 	for(let i = 0; i < subList.length; i++) {
@@ -31,12 +54,6 @@ function buildAutocompleteList(optionList) {
 	}
 	return o;
 }
-
-$.getJSON(API_URL, (res) => {
-	USABLE_POSTS = res.data.children;
-	showNewPost();
-});
-
 function getPost() {
 	let r = Math.floor(Math.random() * USABLE_POSTS.length);
 	let post = USABLE_POSTS[r];
@@ -45,40 +62,11 @@ function getPost() {
 	console.log(post.data);
 	return post.data;
 }
-
 function showNewPost() {
 	const post = getPost();
-	$('#post-title').text(post.title);
-	if(post.thumbnail != 'self' && post.thumbnail != 'default') $('#post-thumbnail').attr('src', post.thumbnail);
-	else $('#post-thumbnail').attr('src', '');
-	$('#post-author').text('/u/' + post.author);
-	$('#post-comments').text(post.num_comments + ' comments');
-	if(post.domain.startsWith('self.')) $('#post-link').text('self.reddit');
-	else $('#post-link').text(post.domain);
-	$('#points').text(points);
-	//console.log(post);
+	$('#post-title').text(post.title);                         // Set title
+	$('#post-thumbnail').attr('src', post.thumbnail);          // Set thumbnail
+	$('#post-author').text('/u/' + post.author);               // Set author
+	$('#post-comments').text(post.num_comments + ' comments'); // Set # of comments
+	post.domain.startsWith('self.') ? $('#post-link').text('self.reddit') : $('#post-link').text(post.domain); // Set post domain (if applicable)
 }
-
-$('#btn-guess').click(() => {
-	if(CURRENT_POST.subreddit.toLowerCase() == $('#guess').val().toLowerCase()) {
-		$('#guess').removeClass('incorrect').removeClass('correct').addClass('correct');
-		points++;
-		wrongs = 0;
-		showNewPost();
-	} else {
-		$('#guess').removeClass('correct').removeClass('incorrect').addClass('incorrect');
-		points--;
-		$('#points').text(points);
-		wrongs++;
-		if(wrongs == 3) {wrongs = 0; showNewPost();}
-
-	}
-
-	$('#guess').val('');
-});
-
-$("#guess").keyup(function(event) {
-    if (event.keyCode === 13) {
-        $("#btn-guess").click();
-    }
-});
